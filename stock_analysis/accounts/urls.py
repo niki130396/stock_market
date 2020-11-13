e14a7e96b90a64bd41d3ecb1c672d5ece7502d1f
niki130396 <niki130396@gmail.com>
2020-11-13 16:31:11 +0200

    added registration option

--- stock_analysis/accounts/urls.py
+++ stock_analysis/accounts/urls.py
@@ -4,4 +4,5 @@ from accounts import views
 
 urlpatterns = [
     path('sign-up/', views.LoginView.as_view(), name='sign-up'),
+    path('register/', views.RegistrationView.as_view(), name='register'),
 ]
--- stock_analysis/accounts/views.py
+++ stock_analysis/accounts/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.contrib.auth import authenticate, login
 from django.http.response import HttpResponseRedirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 
 class LoginView(View):
     def get(self, request):
-        return render(request, 'registration/test_login.html')
+        return render(request, 'registration/login.html')
 
     def post(self, request):
         username = request.POST['username']
@@ -18,3 +19,25 @@ class LoginView(View):
         if user is not None:
             login(request, user)
             return HttpResponseRedirect('/visualisations')
+        return HttpResponseRedirect('/accounts/register')
+
+
+class RegistrationView(View):
+    def get(self, request):
+        return render(request, 'registration/register.html')
+
+    def post(self, request):
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        duplicate_email = User.objects.get(email=email)
+        if not duplicate_email:
+            User.objects.create_user(
+                username=username,
+                email=email,
+                password=password
+                )
+            return HttpResponseRedirect('/visualisations')
+        else:
+            return HttpResponseRedirect('/accounts/sign-up')
